92144a40dd1e3fc9e9c5d437cb192ac8ae251643
Denis Kirsa <kirsenn@ya.ru>
2014-09-10 19:46:33 +0400

    Fix report send

--- korportal.php
+++ korportal.php
@@ -15,10 +15,11 @@ if(isset($argv[1])){
 $protocol = "https";
 $host = "bitrix24.ru";
 $cookiefile = "cookie.txt"; 
-$USER = '';  
-$PASS = '';
-$curl = curl_init();
+$user = '';  
+$pass = '';
+$endDayReport = "";
 
+$curl = curl_init();
 if(!$curl){ print curl_errno($curl); die('Не удалось соединиться');}
 
 $header [] = "Accept:*/*";
@@ -36,14 +37,14 @@ $header [] = "User-Agent:Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.17 
 
 curl_setopt($curl, CURLOPT_URL, "$protocol://$host/?login=yes");
 
-curl_setopt($curl, CURLOPT_VERBOSE , FALSE );
+curl_setopt($curl, CURLOPT_VERBOSE , false);
 curl_setopt($curl, CURLOPT_HEADER , 0 );
 curl_setopt($curl, CURLOPT_HTTPHEADER , $header );
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_COOKIEFILE, $cookiefile);
 curl_setopt($curl, CURLOPT_COOKIEJAR, $cookiefile);
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, "AUTH_FORM=Y&TYPE=AUTH&backurl=%2F&USER_LOGIN=$USER&USER_PASSWORD=$PASS&USER_REMEMBER=Y&Login=%C2%EE%E9%F2%E8" );
+curl_setopt($curl, CURLOPT_POSTFIELDS, "AUTH_FORM=Y&TYPE=AUTH&backurl=%2F&USER_LOGIN=$user&USER_PASSWORD=$pass&USER_REMEMBER=Y&Login=%C2%EE%E9%F2%E8" );
 
 $response = curl_exec($curl);
 $sess_id = preg_replace("/.*'bitrix_sessid':'(.*?)'.*/msi", "$1", $response);
@@ -58,11 +59,11 @@ curl_setopt($curl_ajax, CURLOPT_COOKIEFILE, $cookiefile);
 curl_setopt($curl_ajax, CURLOPT_COOKIEJAR, $cookiefile);
 curl_setopt($curl_ajax, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl_ajax, CURLOPT_HEADER , FALSE );
+
 if($action=="close")
 {
-	$report = "";
 	curl_setopt($curl_ajax, CURLOPT_POST, true);
-	curl_setopt($curl_ajax, CURLOPT_POSTFIELDS, "REPORT=1&ready=Y&" );
+	curl_setopt($curl_ajax, CURLOPT_POSTFIELDS, "REPORT=$endDayReport&ready=Y&" );
 }
 $response_ajax = curl_exec($curl_ajax);
 
